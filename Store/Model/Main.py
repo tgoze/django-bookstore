197f50f451df4baeb96e5768d9a117a4a2c71774
TJ Gozé <tjgoze@gmail.com>
2018-04-03 22:35:59 -0500

    Started templates
    The project is now configured to load templates for the store app and to load static files (CSS, JS, etc.)

--- Store/Model/Main.py
+++ Store/Model/Main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from author_dao import AuthorDao
 from author import Author
 
@@ -10,6 +11,19 @@ if __name__ == '__main__':
     # author.first_name = "George"
     # author.last_name = "Smith"
     # author_dao.update(author)
+=======
+from book_dao import BookDao
+from genre_dao import GenreDao
+from inventory_dao import InventoryDao
+from inventory import Inventory
+
+if __name__ == '__main__':
+    
+    inv = InventoryDao()
+    for x in inv.get_all():
+        print(x.book_id,x.quantity_on_hand,x.quantity_ordered,x.cost,x.retail_price)
+    
+>>>>>>> origin/kara_line
 
     # for x in author_dao.get_all():
     #     print(x.author_id, x.first_name, x.last_name)
