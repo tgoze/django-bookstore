ff83e288192af946ce83f809b9e8a30ba32bb518
TJ Gozé <tjgoze@gmail.com>
2018-04-03 20:51:09 -0500

    Added Materialize CSS and JS

--- Store/Model/Main.py
+++ Store/Model/Main.py
@@ -1,9 +1,17 @@
-from book_dao import BookDao
+from author_dao import AuthorDao
+from author import Author
 
 if __name__ == '__main__':
     
-    book_dao = BookDao()
+    author_dao = AuthorDao()
     
-    for x in book_dao.searchBooks('b'):
-        print(x.bookID, x.title, x.size, x.numberOfPages)
+    # author = Author()
+    # author.author_id = 11
+    # author.first_name = "George"
+    # author.last_name = "Smith"
+    # author_dao.update(author)
 
+    # for x in author_dao.get_all():
+    #     print(x.author_id, x.first_name, x.last_name)
+
+    author_dao.delete(11)
