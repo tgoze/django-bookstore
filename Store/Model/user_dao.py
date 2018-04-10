ca4d7acd028ae99e781d4b76553b9c7fa2f9dd39
kara <karaelliott03@gmail.com>
2018-04-10 09:40:13 -0500

    started login

--- Store/Model/user_dao.py
+++ Store/Model/user_dao.py
@@ -1,7 +1,7 @@
-from user import User 
+from Store.Model.user import User 
+from Store.Model.dbconfig import read_db_config
+from Store.Model.abc_dao import AbcDao
 from mysql.connector import MySQLConnection, Error
-from dbconfig import read_db_config
-from abc_dao import AbcDao
 
 class UserDao(AbcDao):
     def create(self, p_user):
@@ -89,7 +89,7 @@ class UserDao(AbcDao):
         except Exception as e:
             print(e)
         return users
-    def get_by_id(self, id):
+    def get_byid(self, id):
         try:
             
             db_config = read_db_config()        
@@ -98,6 +98,40 @@ class UserDao(AbcDao):
             
             args = [id]
             cursor.callproc('getUserById', args)
+            for result in cursor.stored_results():
+                user = result.fetchall()
+
+            for x in user:
+                u = User()
+                u.id = x[0]
+                u.password = x[1]
+                u.last_login = x[2]
+                u.is_superuser = x[3]
+                u.username = x[4]
+                u.first_name = x[5]
+                u.last_name = x[6]
+                u.email = x[7]
+                u.is_staff = x[8]
+                u.is_active = x[9]
+                u.date_joined = x[10]
+               
+            conn.commit()
+            cursor.close()
+            conn.close()
+        except Error as error:
+            print(error)
+        except Exception as e:
+            print(e)
+        return u
+    def get_byusername(self,username):
+        try:
+            
+            db_config = read_db_config()        
+            conn = MySQLConnection(**db_config)
+            cursor = conn.cursor()
+            
+            args = [username]
+            cursor.callproc('getUserByUserName', args)
             for result in cursor.stored_results():
                 user = result.fetchall()
 
