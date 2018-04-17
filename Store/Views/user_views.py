b2897353db85c60580321e211640993702b2015a
TJ Gozé <tjgoze@gmail.com>
2018-04-16 22:16:19 -0500

    Worked on authentication views

--- Store/Views/user_views.py
+++ Store/Views/user_views.py
@@ -15,7 +15,7 @@ from Store.Model.customer_address import CustomerAddress
 from django.conf import settings
 from django.contrib.auth.hashers import check_password, BCryptPasswordHasher,make_password
 
-from .forms import *
+from Store.forms import *
 from bcrypt import *
 
 class TestView(TemplateView):
@@ -28,6 +28,7 @@ class TestView(TemplateView):
         }
 
         return render(request,self.template_name,context)
+
 
 class AdminCustomerView(TemplateView):
     template_name = 'Store/admin/customers/customers.html'
@@ -42,87 +43,16 @@ class AdminCustomerView(TemplateView):
         return render(request, self.template_name, context)
 
 
-def admin_customer_details(request,customer_id):
+class AdminCustomerDetailView(TemplateView):
+    template_name = 'Store/admin/customers/details.html'
     cdao = CustomerInfoDAO()
-
-    customer = cdao.get_byid(customer_id)
-    customer_address = cdao.get_addressbyid(customer_id)
-    context = {
-        'customer': customer,
-        'caddress': customer_address
-    }
-    return render(request,'Store/admin/customers/details.html', context)
-
-
-class LoginView(TemplateView):
-    user = User()
-    udao = UserDao()
-    login_template = 'Store/customer/login.html'
-    loggedin_template = 'Store/customer/test.html'
-    user.username = 'not logged in'
-
-    def get(self, request):  
-        loginform = LoginForm()  
-        registerform = RegisterUserForm()
+    
+    def get(self, request, customer_id):
+        customer = self.cdao.get_byid(customer_id)
+        customer_address = self.cdao.get_addressbyid(customer_id)
         context = {
-            'loginform': loginform,
-            'registerform': registerform           
+            'customer': customer,
+            'caddress': customer_address
         }
 
-        return render(request, self.login_template, context)
-
-    def post(self, request):
-        #settings.configure(DEBUG=True)
-        user = User()
-        udao = UserDao()
-
-        login_form = LoginForm(request.POST)
-        register_form = RegisterUserForm(request.POST)
-
-        if 'login-user' in request.POST:
-            if login_form.is_valid():
-                user.username = login_form.cleaned_data['username']
-                user.password = login_form.cleaned_data['password']
-                user.id = udao.get_byusername(user.username).id
-                if check_password(user.password,udao.get_byusername(user.username).password):
-                    request.session['user_id'] = user.id
-                    request.session['username'] = user.username
-                    context = {
-                        'text': 'Yay password',
-                        'user_id': request.session['user_id'],
-                        'username': request.session['username']
-                    }                    
-                else:
-                    context = {
-                        'text': 'Bad password bro',
-                        'user_id': None
-                    }    
-            else:
-                context = {
-                    'text': 'try again',
-                    'user_id': None
-                }
-
-            return render(request, self.loggedin_template, context)
-
-        if 'create-user' in request.POST:        
-            if register_form.is_valid():
-                user.first_name = register_form.cleaned_data['first_name']
-                user.last_name = register_form.cleaned_data['last_name']
-                user.email = register_form.cleaned_data['last_name']
-                user.username = register_form.cleaned_data['username']
-                x = register_form.cleaned_data['password']
-                user.is_superuser = 0
-                user.is_active = 1
-                user.is_staff = 0
-                user.password = make_password(x,salt=None,hasher='default')
-                udao.create(user)
-                context = {
-                    'text': 'user saved???'            
-                }
-            else:
-                context = {
-                    'text': 'try again'                    
-                }
-                
-            return render(request,self.login_template, context)
+        return render(request, self.template_name, context)
--- Store/urls.py
+++ Store/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
-# from . import views
+from Store.Views.home_views import *
 from Store.Views.book_views import *
 from Store.Views.user_views import *
-# from .views import AdminCustomerView, LoginView, TestView
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', HomeView.as_view()),
+    path('login', LoginView.as_view()),
     path('admin/books', AdminBookView.as_view()),
     path('admin/books/details/<int:bookID>/', AdminBookDetailView.as_view()),
     path('admin/customers', AdminCustomerView.as_view()),
-    path('admin/customers/details/<int:customer_id>/', views.admin_customer_details(), name='admin_customer_detailss'),
-    path('customer/',(LoginView.as_view())),
+    path('admin/customers/details/<int:customer_id>/', AdminCustomerDetailView.as_view()),
     path('customer/test/', TestView.as_view()),
-    path('customer/',LoginView.as_view())
-
 ]
