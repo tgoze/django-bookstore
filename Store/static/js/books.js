fb3867ab608d2831e5b81013efa5c2ab08e9bba1
TJ Gozé <tjgoze@gmail.com>
2018-04-25 20:33:44 -0500

    Added a search bar

--- Store/static/js/books.js
+++ Store/static/js/books.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('.datepicker').datepicker();
     $('select').formSelect();
     $('.carousel').carousel();
+    $('input#input_text, textarea#textarea2').characterCounter();
 });
 
 var addButtons = document.getElementsByClassName('addbtn')
